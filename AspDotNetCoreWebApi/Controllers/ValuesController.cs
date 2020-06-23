eb3a3c9e2fe3f8b38c97ba18fc5ec0784355bbe1
Bharat Mittal <bharat@aragonindia.com>
2020-06-24 03:01:15 +0530

    Add: Add asp.net core web api and node js api code

---
+++ AspDotNetCoreWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspDotNetCoreWebApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        public ActionResult<string> Get()
+        {
+            return "ASP.NET Core Web API";
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Docker Practical
+
+# run Asp.NetCore Web api on docker
+docker run  -p 81:80/tcp aspdotnetcorewebapi:v1
+
+
+# run NodeJSAPI on docker
+docker run  -p 82:80/tcp nodejsapi:v1
